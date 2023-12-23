ba8143d6161483e9a5df458becd425978b3af1a1
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2023-12-23 14:36:19 +0530

    Add files via upload

---
+++ network.tf
@@ -0,0 +1,25 @@
+
+
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+# # Create a public subnet
+# resource "aws_subnet" "public_subnet" {
+#   vpc_id     = aws_vpc.my_vpc.id
+#   cidr_block = "10.0.1.0/24"
+#   availability_zone = "ap-south-1a" # Replace with your desired availability zone
+
+#   map_public_ip_on_launch = true
+
+#   tags = {
+#     Name = "PublicSubnet"
+#   }
+# }
